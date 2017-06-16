ca212bb09b1b660ba17fd8e31804bc0da1598f99
OmarElraies <omar.elraies@gmail.com>
2017-06-16 08:17:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Logs-Analysis
+# Logs Analysis Project
+
+This Project is part of the Udacity Fullstack Nanodegree curriculum which focus on analyzing data from a web service's logs with practicing command-line and database skills and particularly with a focus on building advanced SQL queries.
+
+## Project Overview
+>In this project, you'll work with data that could have come from a real-world web application, with fields representing information that a web server would record, such as HTTP status codes and URL paths. The web server and the reporting tool both connect to the same database, allowing information to flow from the web server into the report.
+
+## Project Summary
+A reporting tool that will use information from the database to discover what kind of articles the site's readers like.
+
+##Requirements
+
+  * [Python3](https://www.python.org/)
+
+  * [Vagrant](https://www.vagrantup.com/)
+
+  * [VirtualBox](https://www.virtualbox.org/)
+
+
+ ##Usage
+
+	* Download or Clone [fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm) repository.
+
+	* Download or Clone [log-analysis]
+
+	*Unzip this folder after downloading it put the x and y in z diretory
+
+	* Launch the Vagrant VM inside Vagrant sub-directory in the downloaded fullstack-nanodegree-vm repository using command:
+	```
+		$ vagrant up
+	```
+
+	* Log into this using command:
+	```
+		$ vargrant ssh
+	```
+
+	* Change directory to /vagrant
+	* Load the data and connect to database using command:
+	```
+		$ psql -d news -f newsdata.sql
+
+## Running
+	*  Run `python reporting_tool.py`
+
+ ##Output
+
+	```
+	The most popular three articles of all time:
+
+	    "Candidate is jerk, alleges rival" -- 338647 views
+	    "Bears love berries, alleges bear" -- 253801 views
+	    "Bad things gone, say good people" -- 170098 views
+
+	The most popular three authors of all time:
+
+	    Ursula La Multa -- 507594 views
+	    Rudolf von Treppenwitz -- 423457 views
+	    Anonymous Contributor -- 170098 views
+	    Markoff Chaney -- 84557 views
+
+	The days on which more than 1% of requests lead to errors:
+
+	    2016-07-17 -- 2.3% errors
+
+	```
---
+++ reporting_tool.py
@@ -0,0 +1,119 @@
+
+# load the adapter
+import psycopg2
+
+# print the most popular articles of all time
+def print_popular_articles():
+
+    # Define the connection parameters
+    conn_param = "dbname = news"
+
+    # get a connection
+    # if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_param)
+
+    # conn.cursor will return a cursor object
+    cursor = conn.cursor()
+
+    # Define the query to execute
+    query = '''
+            SELECT articles.title, count(*) as views
+                FROM articles, log
+            where log.path =  CONCAT('/article/', articles.slug)
+                and log.status = '200 OK'
+            group by articles.title
+            order by views desc
+            limit 3;
+    '''
+    # execute our Query
+    cursor.execute(query)
+    # retrieve the records from the database
+    records = cursor.fetchall()
+
+    print "\n" + "The most popular three articles of all time:" + "\n"
+    for row in records:
+        print "\"{}\" -- {} views".format(row[0], row[1])
+    # close the connection
+    conn.close()
+
+# print the most popular article authors of all time
+def print_popular_authors():
+
+    # Define the connection parameters
+    conn_param = "dbname = news"
+
+    # get a connection
+    # if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_param)
+
+    # conn.cursor will return a cursor object
+    cursor = conn.cursor()
+
+    # Define the query to execute
+    query = '''
+            SELECT authors.name, count(*) as views
+            FROM authors,articles, log
+            where log.path =  CONCAT('/article/', articles.slug)
+                and log.status = '200 OK'
+                and articles.author = authors.id
+            group by authors.name
+            order by views desc limit 4;
+    '''
+    # execute our Query
+    cursor.execute(query)
+    # retrieve the records from the database
+    records = cursor.fetchall()
+
+    print "\n" + "The most popular three authors of all time:" + "\n"
+    for row in records:
+        print "{} -- {} views".format(row[0], row[1])
+    # close the connection
+    conn.close()
+
+
+# print days on which more than 1% of requests lead to errors
+def print_error_days():
+
+    # Define the connection parameters
+    conn_param = "dbname = news"
+
+    # get a connection
+    # if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_param)
+
+    # conn.cursor will return a cursor object
+    cursor = conn.cursor()
+
+    # Define the query to execute
+
+    query = '''
+        SELECT day, round((error * 1.0/total)*100, 1) as percent
+        from
+            (SELECT date(time) as day,
+                    count(case when status='404 NOT FOUND' then 1 else null end) as error,
+                    count(*) as total
+            from log
+            group by day)
+        as visits
+
+        where ((error*1.0/total)*100) > 1;
+    '''
+
+    # execute our Query
+    cursor.execute(query)
+    # retrieve the records from the database
+    records = cursor.fetchall()
+
+    print "\n" + "The days on which more than 1% of requests lead to errors:" + "\n"
+    for row in records:
+        print "{} -- {}% errors \n".format(row[0], row[1])
+
+
+# print the most popular three articles of all time
+print_popular_articles()
+
+# print the most popular article authors of all time
+print_popular_authors()
+
+# print days on which more than 1% of requests lead to errors
+print_error_days()
