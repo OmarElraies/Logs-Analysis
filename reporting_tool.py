f2cc1af067553ad121c7b5104b65582d0119dec3
OmarElraies <omar.elraies@gmail.com>
2017-06-16 08:50:13 +0200

    Update reporting_tool.py

--- reporting_tool.py
+++ reporting_tool.py
@@ -17,14 +17,14 @@ def print_popular_articles():
 
     # Define the query to execute
     query = '''
-            SELECT articles.title, count(*) as views
-                FROM articles, log
-            where log.path =  CONCAT('/article/', articles.slug)
-                and log.status = '200 OK'
-            group by articles.title
-            order by views desc
-            limit 3;
-    '''
+			SELECT articles.title, count(*) as views
+				FROM articles, log
+			where log.path =  CONCAT('/article/', articles.slug)
+				and log.status = '200 OK'
+			group by articles.title
+			order by views desc
+			limit 3;
+	'''
     # execute our Query
     cursor.execute(query)
     # retrieve the records from the database
@@ -32,7 +32,7 @@ def print_popular_articles():
 
     print "\n" + "The most popular three articles of all time:" + "\n"
     for row in records:
-        print "\"{}\" -- {} views".format(row[0], row[1])
+        print "    \"{}\" -- {} views".format(row[0], row[1])
     # close the connection
     conn.close()
 
@@ -51,14 +51,14 @@ def print_popular_authors():
 
     # Define the query to execute
     query = '''
-            SELECT authors.name, count(*) as views
-            FROM authors,articles, log
-            where log.path =  CONCAT('/article/', articles.slug)
-                and log.status = '200 OK'
-                and articles.author = authors.id
-            group by authors.name
-            order by views desc limit 4;
-    '''
+			SELECT authors.name, count(*) as views
+			FROM authors,articles, log
+			where log.path =  CONCAT('/article/', articles.slug)
+				and log.status = '200 OK'
+				and articles.author = authors.id
+			group by authors.name
+			order by views desc limit 4;
+	'''
     # execute our Query
     cursor.execute(query)
     # retrieve the records from the database
@@ -66,7 +66,7 @@ def print_popular_authors():
 
     print "\n" + "The most popular three authors of all time:" + "\n"
     for row in records:
-        print "{} -- {} views".format(row[0], row[1])
+        print "    {} -- {} views".format(row[0], row[1])
     # close the connection
     conn.close()
 
@@ -87,17 +87,17 @@ def print_error_days():
     # Define the query to execute
 
     query = '''
-        SELECT day, round((error * 1.0/total)*100, 1) as percent
-        from
-            (SELECT date(time) as day,
-                    count(case when status='404 NOT FOUND' then 1 else null end) as error,
-                    count(*) as total
-            from log
-            group by day)
-        as visits
+		SELECT day, round((error * 1.0/total)*100, 1) as percent
+		from
+			(SELECT date(time) as day,
+					count(case when status='404 NOT FOUND' then 1 else null end) as error,
+					count(*) as total
+			from log
+			group by day)
+		as visits
 
-        where ((error*1.0/total)*100) > 1;
-    '''
+		where ((error*1.0/total)*100) > 1;
+	'''
 
     # execute our Query
     cursor.execute(query)
@@ -106,7 +106,7 @@ def print_error_days():
 
     print "\n" + "The days on which more than 1% of requests lead to errors:" + "\n"
     for row in records:
-        print "{} -- {}% errors \n".format(row[0], row[1])
+        print "    {} -- {}% errors \n".format(row[0], row[1])
 
 
 # print the most popular three articles of all time
@@ -117,3 +117,4 @@ print_popular_authors()
 
 # print days on which more than 1% of requests lead to errors
 print_error_days()
+
